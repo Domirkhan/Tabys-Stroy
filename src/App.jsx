f861e10537585e6ee3b89f65b8f3572a944dab47
Damirkhan <damirkhan252006@gmail.com>
2025-02-06 19:37:45 +0500

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './assets/styles/App.css'
 import Header from './assets/layout/Header'
+import Glav from './assets/Pages/Glav'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Glav />
     </>
   )
 }
---
+++ src/assets/Pages/Glav.jsx
@@ -0,0 +1,8 @@
+function Glav() {
+    return(
+        <>
+        
+        </>
+    )
+}
+export default Glav;
