9ae9d2802198884a6006e7c178f3a58108e92d79
Damirkhan <damirkhan252006@gmail.com>
2025-02-06 08:16:10 +0500

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './assets/layout/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
--- src/assets/layout/Header.jsx
+++ src/assets/layout/Header.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import './Header.css';
+
 function Header() {
     return(
         <>
