e9cc07e3204d2e21fad977623aa6118ce2623e30
Damirkhan <damirkhan252006@gmail.com>
2025-02-06 08:17:42 +0500

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import './App.css'
+import './assets/styles/App.css'
 import Header from './assets/layout/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
