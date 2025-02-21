5a86be08a02fdc07b49833fc6e0d093d01da235f
Damirkhan <damirkhan252006@gmail.com>
2025-02-21 21:06:14 +0500

    Fix slider

--- src/assets/components/Slider.jsx
+++ src/assets/components/Slider.jsx
@@ -4,12 +4,12 @@ import "../../assets/styles/Slider.css";
 const slides = [
   {
     image: "https://avatars.mds.yandex.net/i?id=673d9dd7b0697b9b9af7fc359a54cd0f_l-5233858-images-thumbs&n=13",
-    title: "Качественные инструменты от ведущих производителей",
+    title: "Качественные инструменты",
     link: "#"
   },
   {
     image: "https://en.idei.club/uploads/posts/2023-06/1686421166_en-idei-club-p-construction-materials-paint-dizain-krasiv-12.jpg",
-    title: "Все для ремонта и строительства — выгодные цены!",
+    title: "Все для ремонта и строительства",
     link: "#"
   }
 ];
