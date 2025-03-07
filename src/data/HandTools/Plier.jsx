6007c5e158393a61761dde6bf501ec489e9102a2
Erbol Tabys <turkkaz.shopping@gmail.com>
2025-03-07 14:54:12 +0500

    Add tovar

---
+++ src/data/HandTools/Plier.jsx
@@ -0,0 +1,24 @@
+import PlierExpertImage1 from '../../assets/image/HandTools/Плоскогубцы Пасатижи/2.3.1.1.jpg';
+
+
+const Plier = [
+    //  Плоскогубцы, Пасатижы
+                        {
+                            id: 52.1,
+                            name:  "Плоскогубцы Эксперт 51401-180 180мм",
+                            pricePerUnit: {
+                                шт: 2850,
+                                },
+                            // oldPrice: 2850,
+                            image: PlierExpertImage1,
+                            images: [PlierExpertImage1],
+                            // description: "Краска для окон и дверей Dulux",
+                            detailedDescription: "",
+                            specifications: { },
+                            category: "tools", // Категория
+                            subCategory: "Плоскогубцы и пасатижы", // Подкатегория
+                            availability: "Есть в наличии"  // Новое поле для статуса наличия
+                          },
+];
+
+export default Plier;
---
+++ src/data/PowerToolsAccesories/Биты и наворы бит/Nabor.jsx
@@ -0,0 +1,23 @@
+import PlierExpertImage1 from '../../../assets/image/HandTools/Плоскогубцы Пасатижи/2.3.1.1.jpg';
+
+
+const Nabor =[
+    {
+        id: 100,
+        name:  "Плоскогубцы Эксперт 51401-180 180мм",
+        pricePerUnit: {
+            шт: 2850,
+            },
+        // oldPrice: 2850,
+        image: PlierExpertImage1,
+        images: [PlierExpertImage1],
+        // description: "Краска для окон и дверей Dulux",
+        detailedDescription: "",
+        specifications: { },
+        category: "power-tool-accessories", // Категория
+        subCategory: "Наборы", // Подкатегория
+        availability: "Есть в наличии"  // Новое поле для статуса наличия
+    },
+]
+
+export default Nabor;
