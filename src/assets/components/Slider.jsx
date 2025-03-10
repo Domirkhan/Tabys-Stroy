dd2183d83ab44ebdf4d2dbc87d835e2b33f2012c
Damirkhan <damirkhan252006@gmail.com>
2025-03-10 14:50:27 +0500

    слайдер

--- src/assets/components/Slider.jsx
+++ src/assets/components/Slider.jsx
@@ -1,65 +1,56 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom"; // Импорт useNavigate
+import { useNavigate } from "react-router-dom";
+import { useSwipeable } from "react-swipeable"; // Подключаем библиотеку
 import "../../assets/styles/Slider.css";
 
 const slides = [
   {
     image: "https://sotni.ru/wp-content/uploads/2023/08/santekhnika-14.webp",
     title: "НАДЕЖНЫЕ ТРУБЫ ОТ ВЕДУЩИХ ПРОИЗВОДИТЕЛЕЙ",
-    path: "/plumbing/Трубы" // Путь для этого слайда
+    path: "/plumbing/Трубы"
   },
   {
     image: "https://sotni.ru/wp-content/uploads/2023/08/instrumenty-santekhnika-fon-2.webp",
-    title: "фИТИНГИ ДЛЯ ВСЕХ ВИДОВ ТРУБ",
-    path: "/plumbing/Фитинги" // Путь для второго слайда
+    title: "ФИТИНГИ ДЛЯ ВСЕХ ВИДОВ ТРУБ",
+    path: "/plumbing/Фитинги"
   }
 ];
 
 function Slider() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const navigate = useNavigate(); // Хук для переходов
+  const navigate = useNavigate();
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
-    }, 3000);
+    }, 5000);
     return () => clearInterval(interval);
   }, []);
 
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
-  };
+  const handlers = useSwipeable({
+    onSwipedLeft: () => setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length),
+    onSwipedRight: () => setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length),
+    trackMouse: true // Поддержка свайпов мышью (для тестов на ПК)
+  });
 
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
-  };
-
-  // Функция для обработки клика по слайду
-  const handleSlideClick = (path) => {
-    navigate(path); // Переход на нужную страницу
+  const handleSlideClick = () => {
+    navigate(slides[currentIndex].path);
   };
 
   return (
-    <div className="slider">
+    <div className="slider" {...handlers}>
       {slides.map((slide, index) => (
         <div
           key={index}
           className={`slide ${index === currentIndex ? "active" : ""}`}
           style={{ backgroundImage: `url(${slide.image})` }}
-          onClick={() => handleSlideClick(slide.path)} // Добавляем обработчик клика
+          onClick={handleSlideClick}
         >
           <div className="slide-content">
             <h2>{slide.title}</h2>
           </div>
         </div>
       ))}
-
-      {/* <button className="prev" onClick={goToPrevious}>
-        &#10094;
-      </button>
-      <button className="next" onClick={goToNext}>
-        &#10095;
-      </button> */}
 
       <div className="dots">
         {slides.map((_, index) => (
