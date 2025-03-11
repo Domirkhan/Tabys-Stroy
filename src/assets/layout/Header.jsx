bbb6c0c312a1ec48f0158be78cfd775a531051af
Damirkhan <damirkhan252006@gmail.com>
2025-03-11 17:30:33 +0500

    icons

--- src/assets/layout/Header.jsx
+++ src/assets/layout/Header.jsx
@@ -113,6 +113,7 @@ function Header() {
               <img
                 src={Menu}
                 alt="menu"
+                className='menu'
               />
               <span>Каталог</span>
             </div>
@@ -157,7 +158,7 @@ function Header() {
             </div>
             <div className="header-actions">
               <Link to="/cart" className="cart">
-                <img src={cartIcon} alt="cart" />
+                <img src={cartIcon} alt="cart" className='cart-icon'/>
                 <span className="cart-count">{totalQuantity}</span>
               </Link>
             </div>
