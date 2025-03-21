cfb391cd1611350dfc7645625d3f4c3ad58199a6
Damirkhan <damirkhan252006@gmail.com>
2025-03-21 20:25:25 +0500

    fix

--- src/assets/components/BottomNav.jsx
+++ src/assets/components/BottomNav.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { CartContext } from '../../context/CartContext.jsx';
 import homeIcon from '../icon/home.png';
 import catalogIcon from '../icon/Catalog.png';
 import cartIcon from '../icon/cart-2.png';
@@ -7,6 +8,11 @@ import userIcon from '../icon/user.png';
 import '../styles/BottomNav.css';
 
 function BottomNav() {
+  const { cartItems } = useContext(CartContext);
+
+  // Рассчитываем общее количество товаров
+  const totalQuantity = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <nav className="bottom-nav">
       <Link to="/" className="nav-item">
@@ -17,9 +23,12 @@ function BottomNav() {
         <img src={catalogIcon} alt="Каталог" />
         <span>Каталог</span>
       </Link>
-      <Link to="/cart" className="nav-item">
+      <Link to="/cart" className="nav-item bottom-cart-item"> {/* Изменён класс */}
         <img src={cartIcon} alt="Корзина" />
         <span>Корзина</span>
+        {totalQuantity > 0 && (
+          <span className="cart-quantity">{totalQuantity}</span> // Счётчик для корзины
+        )}
       </Link>
       <Link to="/profile" className="nav-item">
         <img src={userIcon} alt="Профиль" />
