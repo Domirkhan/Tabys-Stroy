763b4307292423ce008d8ca56d6a52b01d00f75f
Damirkhan <damirkhan252006@gmail.com>
2025-03-28 03:44:44 +0500

    а

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
+import compression from 'vite-plugin-compression';
+
 
 const manifest = {
   "theme_color": "#ffffff",
