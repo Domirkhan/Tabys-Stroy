53db680563a7885a68543a48edbe5e3eb69b1afc
Damirkhan <damirkhan252006@gmail.com>
2025-04-04 19:44:27 +0500

    fix

--- src/assets/Pages/Product.jsx
+++ src/assets/Pages/Product.jsx
@@ -59,7 +59,7 @@ function Product() {
   };
 
   return (
-    <section className="product-section py-20 bg-light-gray">
+    <section className="product-section-1 py-20 bg-light-gray">
       <div className="container">
         <div className="slider-header">
           <h2 className="section-title text-3xl font-bold text-center mb-12">
