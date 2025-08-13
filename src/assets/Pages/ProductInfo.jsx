286b67617a22a8b6f778af8a3cb67da29b02ab46
Damirkhan <damirkhan252006@gmail.com>
2025-08-13 13:33:57 +0500

    ff

--- src/assets/Pages/ProductInfo.jsx
+++ src/assets/Pages/ProductInfo.jsx
@@ -259,13 +259,13 @@ const ProductInfo = () => {
       </div>
       <hr />
       <div className="container">
+        <h6
+          className="text-center"
+          style={{ fontSize: "1.5rem", fontWeight: "600" }}
+        >
+          Похожие товары
+        </h6>
         <div className="similar-products">
-          <h6
-            className="text-center"
-            style={{ fontSize: "1.5rem", fontWeight: "600" }}
-          >
-            Похожие товары
-          </h6>
           {relatedProducts.length < 1 ? (
             <p className="text-center">нету похожих товаров</p>
           ) : (
