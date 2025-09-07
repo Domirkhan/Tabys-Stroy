e74da85d789ad0844917388620462d0ad9354929
Damirkhan <damirkhan252006@gmail.com>
2025-09-07 21:45:29 +0500

    ff

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'tabys-stroy-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.webmanifest',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
